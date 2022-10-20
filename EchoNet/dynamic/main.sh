44da6a5bc3e6c857bd0bc5b8b8bf7a64cf05e07a
Alice-6161 <3190102199@zju.edu.cn>
2022-10-20 02:55:02 +0000

    update Echo

---
+++ EchoNet/dynamic/main.sh
@@ -0,0 +1,12 @@
+pip install --upgrade --user . 
+# seg
+python echonet/__main__.py segmentation --save_video
+# video
+# erm
+echonet video --batch_size 10 --device cuda --num_workers 0 --num_epochs 20 --mixtype erm --run_test True
+
+# random
+echonet video --batch_size 10 --device cuda --num_workers 0 --num_epochs 20 --mixtype random --run_test True
+
+# kde
+echonet video --batch_size 10 --device cuda --num_workers 0 --num_epochs 20 --mixtype kde --bandwidth 100.0 --run_test True
